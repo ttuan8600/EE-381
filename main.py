2087d95aea455166ddef6306c20fcfe7ead326aa
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-04-23 21:21:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-######  Example 1.11 ##########
+######  EE-381 Final Project ##########
 # load text file
 fname = 'Sales_01_20.csv' # comma separated values
-data1 = np.loadtxt(fname, delimiter=',', skiprows=1)  #skip first row
-#print (data1)
-fig, ax = plt.subplots(1, 2)   # Create a figure containing a single axes
-ax[0].hist(data1, ec='black')
-ax[0].set_title("Equal width bins")
+data = np.loadtxt(fname, delimiter=',', skiprows=1)
+print(data[:5])
 
-ax[1].hist(data1, ec='black', bins=[2, 4, 6, 8, 12, 20, 30])
-ax[1].set_title("Unequal width")
+df = pd.DataFrame(data, columns=['year', 'amount'])
+print(df.head())
 
-ax[0].set_xlabel('Bond strength')
-ax[1].set_xlabel('Bond strength')
-ax[0].set_ylabel('')
+df_byYear = df.groupby('year')['amount'].mean().round(2)
+
+print(df_byYear.head())
+
+df_byYear.plot(kind='bar', xlabel='Year', ylabel='Mean Price', title='Mean Price by Year')
+
 plt.show()
+
+# df = np.loadtxt(fname, delimiter=',', skiprows=1)  #skip first row
+# val = list()
+# for i in range(2):
+#     for j in range(len(data1)):
+#         if data[j][i]
+#         val.append(data1[i][1])
+#
+# avg = round(sum(val)/len(val), 2)
+# print(avg)
+
+# print(avg[:5])
+# fig, ax = plt.subplots(1, 2)   # Create a figure containing a single axes
+# ax[0].hist(data1, ec='black')
+# ax[0].set_title("Equal width bins")
+#
+# ax[1].hist(data1, ec='black', bins=[2, 4, 6, 8, 12, 20, 30])
+# ax[1].set_title("Unequal width")
+#
+# ax[0].set_xlabel('Bond strength')
+# ax[1].set_xlabel('Bond strength')
+# ax[0].set_ylabel('')
+# plt.show()
