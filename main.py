d7ff32e1c2c9b0f27eb27cd835ebf6e658f35b14
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-04-26 18:57:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ######  EE-381 Final Project ##########
+
 # load text file
-fname = 'Sales_01_20.csv' # comma separated values
+fname = 'Sales_01_20.csv'  # comma separated values
+
 data = np.loadtxt(fname, delimiter=',', skiprows=1)
-print(data[:5])
+
+# make an array for the unique year as x-axis
 years = np.unique(data[:, 0])
 
+# make an zeros array with the len as years
 avg_byYear = np.zeros(len(years))
 
+# iterate through the data, get an array of value for each matching year, then avg them, round by 2 decimal
 for i, year in enumerate(years):
     avg_byYear[i] = np.mean(data[data[:, 0] == year, 1]).round(2)
 
+# set the size for the figure contain the bar graph
 fig = plt.figure(figsize=(10, 7))
+
+# create the an array acting for x-axis
 x = [2000, 2005, 2010, 2015, 2020]
 
+# create the bar graph with title, axis labels, and modified tick
 plt.bar(years, avg_byYear)
 plt.xticks(x, x)
 plt.xlabel('Year')
