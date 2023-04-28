cb0305fadac4c54db46711de4a909177223b02bf
Tuan M Tran <81743592+ttuan8600@users.noreply.github.com>
2023-04-27 18:02:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ amount = {}
 for year in years:
     amount[year] = list(data[data[:, 0] == year, 1])
     
-# make an array to store the average amount via year
+# make an array to store and calculate the average amount by year
 avg_byYear = np.zeros(len(years))
 for i, year in enumerate(years):
     avg_byYear[i] = np.mean(amount[year]).round(2)
@@ -28,26 +28,45 @@ for i, year in enumerate(years):
 # create the an array acting for x-axis
 x = [2000, 2005, 2010, 2015, 2020]
 
-# set figure size to hold the graph
-fig = plt.figure(figsize=(7, 5))
+# # set figure size to hold the graph
+# fig = plt.figure(figsize=(7, 5))
 
-# create Yearly Mean the bar graph with title, axis labels, and modified tick
-plt.bar(years, avg_byYear)
-plt.xticks(x, x)
-plt.xlabel('Year')
-plt.ylabel('Mean Price')
-plt.title('Yearly Mean')
-plt.show()
+# # create Yearly Mean the bar graph with title, axis labels, and modified tick
+# plt.bar(years, avg_byYear)
+# plt.xticks(x, x)
+# plt.xlabel('Year')
+# plt.ylabel('Mean Price')
+# plt.title('Yearly Mean')
+# plt.show()
     
-# make an array to store and calulate the STD by year 
-std_byYear = np.zeros(len(years))
-for i, year in enumerate(years):
-    std_byYear[i] = np.std(amount[year]).round(2)
+# # make an array to store and calulate the STD by year 
+# std_byYear = np.zeros(len(years))
+# for i, year in enumerate(years):
+#     std_byYear[i] = np.std(amount[year]).round(2)
 
-# create Yearly STD the bar graph with title, axis labels, and modified tick
-plt.bar(years, std_byYear)
+# # create Yearly STD the bar graph with title, axis labels, and modified tick
+# plt.bar(years, std_byYear)
+# plt.xticks(x, x)
+# plt.xlabel('Year')
+# plt.ylabel('STD')
+# plt.title('Yearly STD')
+# plt.show()
+
+# init a count to count values in between $200,000 - $300,000 (inclusive), 
+# then store and calculate the Probability by 
+c = 0
+prb_byYear = np.zeros(len(years))
+for i, year in enumerate (years):
+    for val in amount[year]:
+        if val >=200000 and val <= 300000:
+            c += 1
+    prb_byYear[i] = round((c/len(amount[year])), 4)
+    c = 0
+    
+# create Yearly Probability the bar graph with title, axis labels, and modified tick
+plt.bar(years, prb_byYear)
 plt.xticks(x, x)
 plt.xlabel('Year')
-plt.ylabel('STD')
-plt.title('Yearly STD')
+plt.ylabel('PRB')
+plt.title('Yearly Probability')
 plt.show()
